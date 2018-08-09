2bd6f1507fdd7bb7d50e8cc1cc2e526004f22685
Brandon Espinoza <espinozab100@gmail.com>
2018-08-08 21:46:01 -0500

    Created controller and added uplaod functionality. Creating products are succesfull

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
-  has_attached_file :smallImage, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :smallImage, content_type: /\Aimage\/.*\z/
+  has_attached_file :small_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/:style/missing.png"
+  validates_attachment_content_type :small_img, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :bigImageOne, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :bigImageOne, content_type: /\Aimage\/.*\z/
+  has_attached_file :big_img_one, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/:style/missing.png"
+  validates_attachment_content_type :big_img_one, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :bigImageTwo, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :bigImageTwo, content_type: /\Aimage\/.*\z/
+  has_attached_file :big_img_two, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "assets/images/:style/missing.png"
+  validates_attachment_content_type :big_img_two, content_type: /\Aimage\/.*\z/
 end
