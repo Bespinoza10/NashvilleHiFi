1d8ea0f7b1cdd7f0c434ace3935e7ed37292ce96
Brandon Espinoza <espinozab100@gmail.com>
2018-09-25 18:24:35 -0500

    created users and only users can create posts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
   end
 
