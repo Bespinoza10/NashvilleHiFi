ce5007c82c26396e51d37fdae3a1c464b8dcf697
Brandon Espinoza <espinozab100@gmail.com>
2018-10-05 17:11:44 -0500

    form is formated correctly with newsletter checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_223036) do
+ActiveRecord::Schema.define(version: 2018_09_26_011847) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.boolean "newsletter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
